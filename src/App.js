0300726a45188f923f7f221188ca3771098c9f78
swatidurgekar <swatidurgekar@gmail.com>
2023-07-21 17:19:53 +0530

    added some text

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Markdown from "react-markdown";
 
 export default function App() {
   const [content, setContent] = useState(
-    "# Welcome to my React Markdown Previewer\n## heading1\nthere's also [links](https://www.freecodecamp.org)\n\nHeres some code, `<div></div>` between 2 backticks.\n\n// this is multi-line code:\n```\nfunction x(){\nconsole.log('Hello');\n}\n```\n- and ofcourse there are lists\n\n> block \n\n ![drawing](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg )\n\nyou can also make text **bold**...whoa"
+    "# Welcome to my React Markdown Previewer\n## heading1\nthere's also [links](https://www.freecodecamp.org)\n\nHeres some code, `<div></div>` between 2 backticks.\n\n// this is multi-line code:\n```\nfunction x(){\nconsole.log('Hello');\n}\n```\n- and ofcourse there are lists\n\n> block \n\nAnd last but not least, let's not forget embedded images:\n\n ![drawing](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg )\n\nyou can also make text **bold**...whoa"
   );
   return (
     <div className="App">
